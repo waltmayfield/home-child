dd6d10706bce3fba8b51f4d53bc2270966270af0
Walt Mayfield <waltmayfield@mac.lan>
2025-11-18 06:33:19 -0600

    Working on data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,33 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
+
+  Activity: a
+    .model({
+      title: a.string().required(),
+      description: a.string().required(),
+      
+    })
+    .authorization((allow) => [allow.authenticated(), allow.guest()]),
+
+  CompletedActivity: a
+    .model({
+      activityID: a.id().required(),
+      completedAt: a.date().required(),
+      feedback: a.customType({
+        rating: a.integer().required(),
+        comments: a.string().required(),
+      })
+    })
+    .authorization((allow) => [allow.owner()]),
+
   Child: a
     .model({
-      name: a.string(),
+      name: a.string().required(),
       sex: a.enum(['male','female']),
-      birthday: a.date(),
+      birthday: a.date().required(),
+      interests: a.string().array(),
+
     })
     .authorization((allow) => [allow.owner()]),
 });
